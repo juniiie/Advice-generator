082688bf2e93162dac5bc1223e1276008bfd2319
Juniiie <jrbuking@gmail.com>
2022-04-29 03:33:32 +1000

    Code refactored

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //Motivational Quotes Objects
 const adviceObjs = {
-  1: "1% everyday",
+  1: "1% better everyday",
   2: "Fail early, Fail Often, but always fail forward",
   3: "Comparing your shortcomings against the strengths of others is a silly thing to do",
   4: "Take charge of your thoughts",
@@ -11,25 +11,19 @@ const adviceObjs = {
   7: "Only when you identify and accept your weaknesses will you finally stop running from your past",
   8: "Distance yourself, for yourself",
   9: "Who checked up on you? Exactly. No one.",
-  10: "Who gives a fuck what other people think? Do you",
+  10: "Your focus is in your control. You lose focus only if you allow it",
   11: "Never put yourself in a situation where you could lose yourself",
-  12: `When you're getting your shit together, it gets lonely. But choose growth over company`,
+  12: `It's on you. Are you going to make today count?`,
   13: `Don't be afraid to grind alone, the sun is always alone and still shines`,
   14: `Sometimes it's good to be alone, gives you time to reflect where you've been`,
   15: `Remember, you didn't come this far just to give up`,
   16: `Sometimes it takes certain things falling apart, for better things to fall into place`,
   17: `Sometimes it takes losing what you're settling for, to remind you of what you truly deserve`,
-  18: `Sometimes it takes the most uncomfortable paths to lead your life to the most beautiful palce`,
+  18: `Sometimes it takes the most uncomfortable paths to lead your life to the most beautiful place`,
   19: `Your current situation is not your final destination`,
   20: `Choose growth over company`,
   21: `You have to understand that there's a process to everthing and you've got to trust it`,
 };
-
-let RandQuoteNumber = Math.floor(
-  Math.random() * Object.keys(adviceObjs).length + 1
-);
-// console.log('Adivce #' +RandQuoteNumber);
-// console.log(adviceObjs[`${RandQuoteNumber}`]);
 
 const btnClick = function () {
   let RandQuoteNumber = Math.floor(
@@ -48,4 +42,3 @@ const btnClick = function () {
 //console.log(Object.keys(adviceObjs).length); //gets length of objects
 
 document.querySelector(".main__cardCircle").addEventListener("click", btnClick);
-document.querySelector("#advText").addEventListener("load", btnClick);
